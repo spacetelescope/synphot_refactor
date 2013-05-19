2f8a9b422674a1ead65f4d205ac077ba214b126e
Christopher Hanley <chrishanley@astronomyworks.com>
2013-05-19 00:18:42 -0400

    Change blind exception to one for catching ImportErrors.

--- pysynphot/svn_version.py
+++ pysynphot/svn_version.py
@@ -5,6 +5,6 @@ try:
     from .version import *
     __svn_version__ = __svn_revision__
     __full_svn_info__ = __svn_full_info__
-except:
+except ImportError as e:
     __svn_version__ = ''
     __full_svn_info__ = ''
