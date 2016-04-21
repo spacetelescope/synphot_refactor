1526e08cb5829c22e5a25b92901a15000ef35a71
Pey Lian Lim <lim@stsci.edu>
2016-04-21 11:17:53 -0400

    Added STScI logo to sidebar

--- docs/conf.py
+++ docs/conf.py
@@ -107,6 +107,9 @@ release = package.__version__
 # Custom sidebar templates, maps document names to template names.
 #html_sidebars = {}
 
+# Logo
+html_logo = '_static/stsci_logo.png'
+
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
