a4dccf9e0271024d2d73d956b850b6c78d2f05b9
P. L. Lim <lim@stsci.edu>
2017-08-05 12:05:14 -0400

    Fix failed tests due to Astropy constants update (#128)
    Use old constants for tests to maintain backward compatibility with astrolib results

--- synphot/tests/test_spectrum.py
+++ synphot/tests/test_spectrum.py
@@ -12,6 +12,7 @@ import numpy as np
 import pytest
 
 # ASTROPY
+import astropy
 from astropy import modeling
 from astropy import units as u
 from astropy.io import fits
@@ -40,9 +41,31 @@ else:
     HAS_SCIPY = True
 
 HAS_SCIPY = HAS_SCIPY and minversion(scipy, '0.14')
+ASTROPY_LT_20 = not minversion(astropy, '2.0')
 
 # GLOBAL VARIABLES
 _vspec = None  # Loaded in test_load_vspec()
+
+
+def setup_module(module):
+    # https://github.com/astropy/astropy/issues/6383
+    if not ASTROPY_LT_20:
+        import astropy.constants as const
+        from astropy.constants import si, astropyconst13
+
+        const.sigma_sb = si.sigma_sb = astropyconst13.sigma_sb
+        const.h = si.h = astropyconst13.h
+        const.k_B = si.k_B = astropyconst13.k_B
+
+
+def teardown_module(module):
+    # https://github.com/astropy/astropy/issues/6383
+    if not ASTROPY_LT_20:
+        import astropy.constants as const
+        from astropy.constants import si, astropyconst20
+        const.sigma_sb = si.sigma_sb = astropyconst20.sigma_sb
+        const.h = si.h = astropyconst20.h
+        const.k_B = si.k_B = astropyconst20.k_B
 
 
 @remote_data
