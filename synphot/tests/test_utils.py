c69035e930a53091f22c04c2c79ca95b0ee97a8a
Pey Lian Lim <2090236+pllim@users.noreply.github.com>
2020-03-17 16:37:54 -0400

    TST: Ignore warning on Windows

--- synphot/tests/test_utils.py
+++ synphot/tests/test_utils.py
@@ -34,6 +34,7 @@ def test_validate_totalflux():
     utils.validate_totalflux(0.01 * units.FLAM)
 
 
+@pytest.mark.filterwarnings('ignore:invalid value')
 @pytest.mark.parametrize(
     'val', (-0.01, -0.01 * units.FLAM,
             0, 0 * units.PHOTLAM,
