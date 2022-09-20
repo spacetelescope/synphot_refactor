815f5881c52e989026842b286b39f0ccc890d6a2
Zach Burnett <zburnett@stsci.edu>
2022-09-20 13:03:48 -0400

    use canonical filename of RTD configuration and enable `nitpicky` mode

--- docs/conf.py
+++ docs/conf.py
@@ -138,3 +138,6 @@ linkcheck_retry = 5
 linkcheck_ignore = ['https://hsthelp.stsci.edu']
 linkcheck_timeout = 180
 linkcheck_anchors = False
+
+# Enable nitpicky mode - which ensures that all references in the docs resolve.
+nitpicky = True
